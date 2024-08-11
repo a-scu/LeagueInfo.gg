136a6637842d18347b149a94a645ca90c1cd5b61
a-scu <scuderialexis2001@gmail.com>
2024-08-11 18:17:53 -0300

    probando expand button mas chico

--- src/components/recentGames/collapsedView/GameInfo.jsx
+++ src/components/recentGames/collapsedView/GameInfo.jsx
@@ -1,7 +1,7 @@
 export default function GameInfo({ win, queue, gameDuration, gameAge }) {
   return (
     <div
-      className={`truncate gap-2 py-1.5 max-800:border-b 800:py-1.5 items-center 800:items-start p-2 max-410:pr-[46px] pr-16 800:px-0 left-0 max-800:pl-2.5 pl-4 800:static w-full justify-between absolute top-0 flex-row flex 800:flex-col 800:w-28 800:h-full text-2xs 800:text-xs text-gray-6 ${
+      className={`truncate gap-2 max-500:py-1 py-1.5 max-800:border-b 800:py-1.5 items-center 800:items-start p-2 max-410:pr-[46px] pr-16 800:px-0 left-0 max-800:pl-2.5 pl-4 800:static w-full justify-between absolute top-0 flex-row flex 800:flex-col 800:w-28 800:h-full text-2xs 800:text-xs text-gray-6 ${
         win ? "border-db-300 bg-db-100" : "border-dr-300 bg-dr-100"
       }`}
     >
