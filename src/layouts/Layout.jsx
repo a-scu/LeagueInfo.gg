21ca9bf5e45615be65a3b7a8699b50f24d4dcffa
Mascocitasapp <consultasmascocitas@gmail.com>
2025-02-21 19:56:03 -0300

    probando de nuevo

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,14 +1,9 @@
-import { useStore } from "@nanostores/react";
-import { $theme } from "@/js/store";
-
 import Header from "@/components/header/Header";
 import Footer from "@/components/footer/Footer";
 
 export default function Layout({ children }) {
-  const theme = useStore($theme);
-
   return (
-    <div className={theme}>
+    <div>
       <div className="min-h-screen">
         <Header />
         {children}
